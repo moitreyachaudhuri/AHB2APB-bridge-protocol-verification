f35c24a29f6b7fa260833e8bd94ac1c1149c5c30
moitreyachaudhuri <111184171+moitreyachaudhuri@users.noreply.github.com>
2024-10-10 02:15:37 +0530

    Update apb_driver.v

--- apb_driver.v
+++ apb_driver.v
@@ -36,3 +36,28 @@ function void apb_driver::connect_phase(uvm_phase phase);
 	super.connect_phase(phase);
 	vif=apb_cfg.vif;
 endfunction
+task apb_driver::run_phase(uvm_phase phase);
+
+        req = apb_xtn::type_id::create("req", this);
+
+        forever
+                begin
+                        //seq_item_port.get_next_item(req);
+                        send_to_dut(req);
+                        //req.print();
+                        //seq_item_port.item_done();
+                end
+endtask
+
+///----send to dut------/////
+task apb_driver::send_to_dut(apb_xtn xtn);
+        wait(vif.apb_drv_cb.Pselx != 0); //wait for Psel to be high
+
+        if(vif.apb_drv_cb.Pwrite == 0) //the moment Psel is high, check for Pwrite signal
+                vif.apb_drv_cb.Prdata <= {$random};
+
+        repeat(2) @(vif.apb_drv_cb); //wait for 2 cycles b4 generating next xtn
+        //`uvm_info("apb_driver", "Displaying apb_driver data", UVM_LOW)
+ //       xtn.print();
+        apb_cfg.drv_data_count++;
+endtask
